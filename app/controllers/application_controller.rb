c689af8bb622680984488dcf753fd0ccbf46d730
Audrea Cook <audreacwork@gmail.com>
2020-04-01 15:43:15 -0600

    Add way to find current user
    Following tutorial here: https://suncoast.io/handbook/resources/rails/add-auth0-to-react-rails-app/

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+
+  private
+  # find the current active user from Auth0
+  def current_user
+    @user ||= begin
+                token = request.headers["Authorization"].to_s.split(" ").last
+                payload, header = *JSONWebToken.verify(token)
+                User.from_auth_hash(payload)
+              end
+  rescue JWT::VerificationError, JWT::DecodeError
+    nil
+  end
 end
