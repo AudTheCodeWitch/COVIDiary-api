7285d4f51cc1455c06f7682c1f348411eaf50c34
Audrea Cook <audreacwork@gmail.com>
2020-04-01 15:03:57 -0600

    Revert "Seed database"
    This reverts commit ef3678e4

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,32 +10,37 @@ AdminUser.create!(email: 'admin@example.com', password: 'password', password_con
 
 #if Rails.env.development?
 
-# create Users
-count = 0
-10.times do
-  user = User.create :email => "name#{count}@example.com", :password => 'topsecret', :password_confirmation => 'topsecret'
-  # create Profile for user
-  Profile.create(
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.unique.last_name,
-      birthdate: Faker::Date.between(from: 100.years.ago, to: Date.today),
-      occupation: Faker::Job.title,
-      essential: Faker::Boolean.boolean,
-      isolation_start: Faker::Date.between(from: 3.months.ago, to: Date.today),
-      about: Faker::Lorem.paragraphs(number: 2),
-      user_id: user.id)
-
-  # create 3 diary Entries for user
-  3.times do
-    Entry.create(
-        health_rating: Faker::Number.between(from: 1, to: 5),
-        symptoms_present?: Faker::Boolean.boolean,
-        health_comments: Faker::Lorem.paragraphs(number: 1),
-        mental_health_rating: Faker::Number.between(from: 1, to: 5),
-        mental_health_comments: Faker::Lorem.paragraphs(number: 1),
-        diary_entry: Faker::Lorem.paragraphs(number: 4),
-        user_id: user.id)
+  # create Users
+  10.times do
+    count = 0
+    user = User.create! :email => "name#{count}@example.com", :password => 'topsecret', :password_confirmation => 'topsecret'
+    user.skip_confirmation!
+    user.save!
+    count += 1
   end
 
-  count += 1
-end
+  User.all do |u|
+    # create Profiles
+    Profile.create(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.unique.last_name,
+        birthdate: Faker::Date.between(from: 100.years.ago, to: Date.today),
+        occupation: Faker::Job.title,
+        essential: Faker::Boolean.boolean,
+        isolation_start: Faker::Date.between(from: 3.months.ago, to: Date.today),
+        about: Faker::Lorem.paragraphs(number: 2),
+        user_id: u.id)
+
+    # create 3 entries per user
+    3.times do
+      Entry.create(
+          health_rating: Faker::Number.between(from: 1, to: 5),
+          symptoms_present?: Faker::Boolean.boolean,
+          health_comments: Faker::Lorem.paragraphs(number: 1),
+          mental_health_rating: Faker::Number.between(from: 1, to: 5),
+          mental_health_comments: Faker::Lorem.paragraphs(number: 1),
+          diary_entry: Faker::Lorem.paragraphs(number: 4),
+          user_id: u.id)
+    end
+  end
+# end
