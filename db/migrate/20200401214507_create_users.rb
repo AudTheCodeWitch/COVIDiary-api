8de7339914b98e0188a2f20d272975f13e4cdc13
Audrea Cook <audreacwork@gmail.com>
2020-04-01 16:05:30 -0600

    Add User Model
    Following tutorial here: https://suncoast.io/handbook/resources/rails/add-auth0-to-react-rails-app/

---
+++ db/migrate/20200401214507_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :auth_sub
+      t.string :first_name
+      t.string :last_name
+      t.datetime :birth_date
+      t.string :occupation
+      t.boolean :essential
+      t.datetime :isolation_start
+      t.datetime :isolation_end
+      t.text :about
+
+      t.timestamps
+    end
+  end
+end
