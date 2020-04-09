a125d69ab8a8285c1306eb718d4f71305d65456e
Audrea Cook <audreacwork@gmail.com>
2020-04-09 16:00:48 -0600

    Migrate data

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 2020_04_09_214926) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "first_name"
     t.string "last_name"
