d87ffadcd162998b8ae1fe3e69dc2f61fd4556ef
Audrea Cook <audreacwork@gmail.com>
2020-04-16 11:31:27 -0600

    Nest entries under users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :entries
-    resources :users
+    resources :users do
+      resources :entries
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
