3503630b678e5d4db208cb650370da616dd1d924
Audrea Cook <audreacwork@gmail.com>
2020-04-16 11:36:21 -0600

    Stub user controller actions

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,41 @@
 class Api::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: { error: "Error creating user" }
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(
+        :id,
+        :first_name,
+        :last_name,
+        :email,
+        :birth_date,
+        :occupation,
+        :essential?,
+        :isolation_start,
+        :isolation_end,
+        :about
+    )
+  end
 end
