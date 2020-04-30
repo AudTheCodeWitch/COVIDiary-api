c8797a788ce04dc8aa694d3ea0e5220a659f4de9
Audrea Cook <audreacwork@gmail.com>
2020-04-30 09:53:00 -0600

    Add default value to is_public boolean

--- db/migrate/20200423204851_add_is_public_to_entries.rb
+++ db/migrate/20200423204851_add_is_public_to_entries.rb
@@ -1,5 +1,5 @@
 class AddIsPublicToEntries < ActiveRecord::Migration[6.0]
   def change
-    add_column :entries, :is_public, :boolean
+    add_column :entries, :is_public, :boolean, default: false
   end
 end
